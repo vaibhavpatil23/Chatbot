489760952ea35dd56fca6e380704846131b69505
vaibhav <vpatil7382@gmail.com>
2023-06-22 20:15:19 +0530

    code change

--- script.js
+++ script.js
@@ -80,6 +80,6 @@ chatInput.addEventListener("keydown", (e) => {
     }
 });
 
-sendChatBtn.addEventListener("click", handleChat);
+sendChatBtn.addEventListener("click", handleChat)
 closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
 chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
